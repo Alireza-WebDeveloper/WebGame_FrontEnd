f7bd654e694dcc0c49758b7ed8d000789aa74e70
alireza-webdeveloper <alirezakh0090@gmail.com>
2024-02-07 17:40:09 +0330

    Update Landing Title

--- View/GameLanding/index.js
+++ View/GameLanding/index.js
@@ -42,16 +42,23 @@ const generateGameLandingList = (data) => {
 };
 
 const generateViewMoreLink = () => {
-  return `<div class='flex justify-center'>
+  return `<div class='flex justify-center mt-3'>
     <a href='/game' type="button" class="text-white capitalize text-lg bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2">موارد بیشتر</a>
    </div>`;
+};
+
+const generateTitle = () => {
+  return `<h1 class="text-3xl font-semibold capitalize text-center animate-pulse">
+    با ما بهترین را تجربه کنید
+  </h1>`;
 };
 
 // Render
 const render = (data) => {
   document.querySelector('main').appendChild(parElement);
   parElement.innerHTML = `
-    <div class="grid grid-cols-4 grid-rows-5 h-[150vh] gap-y-2 gap-x-3 p-2">
+    ${generateTitle()}
+    <div class="grid grid-cols-4 grid-rows-5 h-[150vh] gap-y-2 gap-x-3 p-2 mt-10">
       ${generateGameLandingList(data)}
     </div>
     ${generateViewMoreLink()}`;
