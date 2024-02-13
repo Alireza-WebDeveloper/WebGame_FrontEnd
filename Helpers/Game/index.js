9c6d76ec6fa5d6f0deff0af431c8e300d1d3da1a
alireza-webdeveloper <alirezakh0090@gmail.com>
2024-02-13 16:35:54 +0330

    Add Query Search On Game Page

--- Helpers/Game/index.js
+++ Helpers/Game/index.js
@@ -1,7 +1,22 @@
 import RequestApi from '../Base';
+
 const getAllGameList = async () => {
   try {
-    const response = await RequestApi.get('/game');
+    // 1 ) GET URL SITE
+    const currentUrl = new URL(window.location.href);
+    // 2 ) Read Query Of Url
+    const platform = currentUrl.searchParams.get('platform');
+    const genre = currentUrl.searchParams.get('genre');
+
+    const queryParams = new URLSearchParams();
+    if (platform) queryParams.append('platform', platform);
+    if (genre) queryParams.append('genre', genre);
+    // 3 ) Condition
+    const url =
+      '/game' + (queryParams.toString() ? '?' + queryParams.toString() : '');
+    // 4 ) Request
+    const response = await RequestApi.get(url);
+    // 5 ) Response
     return response.data;
   } catch (err) {
     throw new Error(err);
--- View/Game/index.js
+++ View/Game/index.js
@@ -1,3 +1,5 @@
+import LazyLoad from 'vanilla-lazyload';
+
 const parElement = document.querySelector('#game');
 
 const generateGenre = (data) => {
@@ -11,19 +13,18 @@ const generateGenre = (data) => {
 const generateGameList = (data) => {
   return data
     .map((game) => {
-      return `<a href='/game/${game._id}' class='
-       shadow-md  shadow-gray-900 
-      flex rounded-md relative'>
-      <img class='h-60 
-      rounded-md w-full' src='${game.bannerImage}' alt='not found'/>
-      <div class='absolute z-20 p-4 top-0 left-0 text-white w-full h-full rounded
-       flex flex-col space-y-3 justify-end
-      '>
-      <div>
-      ${generateGenre(game.genre)}
-      </div>
-       <h1 class=' font-semibold text-xl'>${game.name}</h1>
-       <p>${game.description.slice(0, 100)}...</p>
+      return `<a href='/game/${
+        game._id
+      }' class='shadow-md  shadow-gray-900 flex rounded-md relative'>
+      <img class='h-60 rounded-md w-full lazy' data-src='${
+        game.bannerImage
+      }' alt='not found'/>
+      <div class='absolute z-20 p-4 top-0 left-0 text-white w-full h-full rounded flex flex-col space-y-3 justify-end'>
+        <div>
+          ${generateGenre(game.genre)}
+        </div>
+        <h1 class='font-semibold text-xl'>${game.name}</h1>
+        <p>${game.description.slice(0, 100)}...</p>
       </div>
       <div class='absolute inset-0 z-10 top-0 bg-gray-800 opacity-20 rounded'></div>
       </a>`;
@@ -31,10 +32,15 @@ const generateGameList = (data) => {
     .join('');
 };
 
+const loading = () => {};
+
 const render = (data) => {
   parElement.innerHTML = `
    ${generateGameList(data)}
   `;
+  new LazyLoad({
+    elements_selector: '.lazy',
+  });
 };
 
-export { render };
+export { render, loading };
