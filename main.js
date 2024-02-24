6493c5ea9d010b30c6d20b5dc2a78ae4cfcc5f48
alireza-webdeveloper <alirezakh0090@gmail.com>
2024-02-24 11:49:32 +0330

    Update Performance

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+// !! Styles
 import './style.css';
-import * as HeaderMainView from './View/Header/main';
-import * as HeaderGameView from './View/Header/game';
+
+// !! Controllers
 import * as PlatformController from './Controller/Platform';
 import * as GameLandingController from './Controller/GameLanding';
 import * as GenreController from './Controller/Genre';
 import * as GameController from './Controller/Game';
-import * as NotFoundView from './View/NotFound';
-import * as ThemeView from './View/Theme';
 import * as GameDetailController from './Controller/GameDetail';
 import * as GameFavoriteController from './Controller/GameFavorite';
+
+// !! View
+import * as HeaderMainView from './View/Header/main';
+import * as HeaderGameView from './View/Header/game';
+import * as NotFoundView from './View/NotFound';
+import * as ThemeView from './View/Theme';
+
+// !! RegExp
 const regexPattern = '\\/game\\/(\\d+)';
 const regex = new RegExp(regexPattern);
 
+// !! Route
 const Route = async (currentPath = window.location.pathname) => {
   if (currentPath === '/') {
     HeaderMainView.render();
