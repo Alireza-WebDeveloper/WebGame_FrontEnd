2c0e73c867ca89ccea5c9080152ee7395353384a
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-04-30 12:59:16 +0330

    Update Request

--- Helpers/Base/index.js
+++ Helpers/Base/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-// const RequestApi = axios.create({
-//   baseURL: 'http://localhost:8000/api',
-// });
-
 const RequestApi = axios.create({
-  baseURL: 'https://web-game-backed.vercel.app/api',
+  baseURL: 'http://localhost:8000/api',
 });
+
+// const RequestApi = axios.create({
+//   baseURL: 'https://web-game-backed.vercel.app/api',
+// });
 
 export default RequestApi;
