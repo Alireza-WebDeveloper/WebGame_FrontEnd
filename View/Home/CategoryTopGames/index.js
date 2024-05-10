07560afce29e8ec7e1d4d01f9658f8d4cb6eea09
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-05-10 11:33:58 +0330

    Update Responsive

--- View/Home/CategoryTopGames/index.js
+++ View/Home/CategoryTopGames/index.js
@@ -2,7 +2,7 @@ const parElement = document.querySelector('#category_TopGames');
 
 // !! Generate Category Top Games
 const render = () => {
-  parElement.innerHTML = ` <div class="grid md:grid-cols-2 grid-cols-1 gap-5">
+  parElement.innerHTML = ` <div class="grid md:grid-cols-2 grid-cols-1  gap-5 mt-2">
     <a href='/topGames/2024'>
     <section
       class="p-5 h-44 flex justify-center items-center cursor-pointer shadow-md shadow-black text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
