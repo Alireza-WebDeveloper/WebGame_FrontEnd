0ce93e9d048a0122594a0383535b8861ef5bf593
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-05-10 19:18:26 +0330

    Update Ui

--- View/Home/GameSearch/index.js
+++ View/Home/GameSearch/index.js
@@ -5,7 +5,7 @@ const inputValue = () => gameSearch.querySelector('input').value;
 const render = () => {
   gameSearch.innerHTML = `<div class="relative">
   <span class="absolute right-2 top-1/2 transform cursor- hover:bg-gray-50 cursor-pointer -translate-y-1/2 bg-gray-100 dark:bg-gray-600 h-8 w-8 rounded-full flex items-center justify-center"><i class="fa fa-search fa-lg" aria-hidden="true"></i></span>
-  <input class="appearance-none  h-12 block w-full text-left bg-white dark:border-slate-400 dark:text-white dark:bg-slate-700 text-gray-700 border  rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200" id="grid-first-name" type="text">
+  <input autocomplete="off"  class="appearance-none  h-12 block w-full text-left bg-white dark:border-slate-400 dark:text-white dark:bg-slate-700 text-gray-700 border  rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200" id="grid-first-name" type="text">
 </div>`;
 };
 
